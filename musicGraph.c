31475fbb511801b0bfa9dd899034854c9ce437b5
MPKaronde <karonde.manav@gmail.com>
2025-01-15 20:25:40 -0600

    initial commit

---
+++ musicGraph.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Graph of albums and genres.
+Both genres and albums are represented as nodes. Albums are only adjacent to genres they contain and vice versa
+Edge weights (0 - 1) represent how closely connected a song and a genre are intertwined
+*/
+
+// Nodes
+typedef struct
+{
+
+} Node;
